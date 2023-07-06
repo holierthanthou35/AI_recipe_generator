8a2ffee784329a29a89a7c219356138c0b9a2180
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-07-06 22:32:47 +0530

    feat(backend): added backend to listen to /item and add them to items.json
    page reloads everytime api is fetched

---
+++ pantry/pantry.js
@@ -0,0 +1,36 @@
+const pantryBtns = document.querySelectorAll(".pantry-btn");
+const shoppingList = document.getElementById("shopping-list");
+
+pantryBtns.forEach(btn => {
+	btn.addEventListener("click", () => {
+		const listItem = document.createElement("li");
+		const itemContent = btn.dataset.content;
+		const itemContentArray = itemContent.split(" ");
+
+		for (var i = 0; i < itemContentArray.length; i++) {
+			itemContentArray[i] =
+				itemContentArray[i].charAt(0).toUpperCase() +
+				itemContentArray[i].slice(1);
+		}
+
+		const newItemContent = itemContentArray.join(" ");
+
+		listItem.textContent = newItemContent;
+		shoppingList.appendChild(listItem);
+
+        const postItem = async () => {
+            const rawResponse = await fetch('http://localhost:5000/item', {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({item: newItemContent})
+            });
+            const content = await rawResponse.json();
+          
+            console.log(content);
+        }
+        postItem();
+	});
+});
