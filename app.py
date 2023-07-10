528682eb96087e46164cff64ed1e1b5e543a13ff
Pranav <pranavprajapati586@gmail.com>
2023-07-10 20:45:30 +0530

    feat: Flask templated to render in frontend

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<h1> Hello world </h1>"
+food_items = []
+
+@app.route("/",methods=['POST','GET'])
+def getitems():
+    if request.method == 'POST':
+        data = request.form.get('new_item')
+        food_items.append(data)
+        return render_template('index.html',food_items = food_items)
+    return render_template('index.html',food_items=food_items)
 
 if __name__ == '__main__':
     app.run(debug=True)
